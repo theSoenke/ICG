d77d156e6af7724388357fd8cc9617f729fc18d6
theSoenke <theSoenke@users.noreply.github.com>
2015-11-08 15:20:54 +0100

    Rechtecke können gezeichnet und gefärbt werden

--- uebung2/rectangle.js
+++ uebung2/rectangle.js
@@ -1,53 +1,133 @@
 var gl;
+var mouseDown;
+var mouseUp;
+var program;
 
-window.onload = function init()
-{
-	// Get canvas and setup webGL
+window.onload = function init() {
+  // Get canvas and setup webGL
+  var canvas = document.getElementById("gl-canvas");
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
 
-	var canvas = document.getElementById("gl-canvas");
-	gl = WebGLUtils.setupWebGL(canvas);
-	if (!gl) { alert("WebGL isn't available"); }
+  // Configure viewport
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(0, 0, 0, 1.0);
 
-	// Specify position and color of the vertices
+  // Init shader program and bind it
+  program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
 
-	var vertices = new Float32Array();
-	var colors = new Float32Array();
+  var colors = new Float32Array([1, 1, 1, 1,
+    1, 1, 1, 1,
+    1, 1, 1, 1,
+    1, 1, 1, 1
+  ]);
 
-	// Configure viewport
+  setColorBuffer(colors);
 
-	gl.viewport(0,0,canvas.width,canvas.height);
-	gl.clearColor(1.0,1.0,1.0,1.0);
+  // add listener
+  canvas.addEventListener("mousedown", eventMouseDown);
+  canvas.addEventListener("mouseup", eventMouseUp);
+  window.addEventListener("keypress", setColor);
 
-	// Init shader program and bind it
-
-	var program = initShaders(gl, "vertex-shader", "fragment-shader");
-
-	gl.useProgram(program);
-
-	// Load colors into the GPU and associate shader variables
-
-	var cBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
-
-	var vColor = gl.getAttribLocation(program, "vColor");
-	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(vColor);
-
-	// Load positions into the GPU and associate shader variables
-
-	var bufferId = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-	gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-	var vPosition = gl.getAttribLocation(program, "vPosition");
-	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(vPosition);
-	render();
+  render();
 };
 
-function render()
-{
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.TRIANGLES, 0, 0);
+function eventMouseDown(e) {
+  mouseDown = e;
+}
+
+function eventMouseUp(e) {
+  mouseUp = e;
+  drawRectangle();
+}
+
+function drawRectangle() {
+  var vertices = new Float32Array([
+    normPos(mouseDown.clientX), normPos(mouseDown.clientY),
+    normPos(mouseDown.clientX), normPos(mouseUp.clientY),
+    normPos(mouseUp.clientX), normPos(mouseDown.clientY),
+    normPos(mouseUp.clientX), normPos(-mouseUp.clientY)
+  ]);
+
+  console.log(vertices);
+
+  var colors = new Float32Array([1, 0, 0, 1,
+    0, 1, 0, 1,
+    0, 0, 1, 1,
+    1, 1, 0, 1
+  ]);
+
+  setVerticesBuffer(vertices)
+}
+
+function setColor(e) {
+  var colors;
+
+  if (e.key.toLowerCase() == "r") {
+    colors = new Float32Array([1, 0, 0, 1,
+      1, 0, 0, 1,
+      1, 0, 0, 1,
+      1, 0, 0, 1
+    ]);
+  } else if (e.key.toLowerCase() == "g") {
+    colors = new Float32Array([0, 1, 0, 1,
+      0, 1, 0, 1,
+      0, 1, 0, 1,
+      0, 1, 0, 1
+    ]);
+  } else if (e.key.toLowerCase() == "b") {
+    colors = new Float32Array([0, 0, 1, 1,
+      0, 0, 1, 1,
+      0, 0, 1, 1,
+      0, 0, 1, 1
+    ]);
+  } else {
+    colors = new Float32Array([1, 1, 1, 1,
+      1, 1, 1, 1,
+      1, 1, 1, 1,
+      1, 1, 1, 1
+    ]);
+  }
+
+  setColorBuffer(colors);
+}
+
+function setVerticesBuffer(vertices) {
+  var bufferId = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  var vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+}
+
+function setColorBuffer(colors) {
+  var cBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+  var vColor = gl.getAttribLocation(program, "vColor");
+  gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vColor);
+}
+
+function normPos(value) {
+  return normValue(value, 0, 512, -1, 1);
+}
+
+function normValue(value, valueMin, valueMax, resultMin, resultMax) {
+  value = Math.abs(value) - (valueMax / 2);
+
+  return ((((value) - (valueMin)) / ((valueMax) - (valueMin))) *
+    (((resultMax) - (resultMin)) + (resultMin)));
+}
+
+function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+  requestAnimFrame(render);
 }
