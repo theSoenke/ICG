c71df4f05135a9f6cb376da7fcb01dd8378bff93
theSoenke <theSoenke@users.noreply.github.com>
2015-12-27 12:17:16 +0100

    OOP Ansatz verwendet

--- uebung07/main.js
+++ uebung07/main.js
@@ -5,6 +5,8 @@ var eye;
 var target;
 var up;
 
+var objects = [];
+
 window.onload = function init() {
   // Get canvas and setup webGL
 
@@ -26,41 +28,62 @@ window.onload = function init() {
 
   defaultProgram = initShaders(gl, "vertex-shader", "fragment-shader");
 
-  // Create cube
-  var cubeString = document.getElementById("cube.obj").innerHTML;
-  cubeMesh = new OBJ.Mesh(cubeString);
-  OBJ.initMeshBuffers(gl, cubeMesh);
-
-  cubeModelMatrix = mat4.create();
-
   // Create beach
   var beachStr = document.getElementById("plane.obj").innerHTML;
   beachMesh = new OBJ.Mesh(beachStr);
   OBJ.initMeshBuffers(gl, beachMesh);
 
-  beachModelMatrix = mat4.create();
-	mat4.translate(beachModelMatrix, beachModelMatrix, vec3.fromValues(25, -3, -30));
-	mat4.scale(beachModelMatrix, beachModelMatrix, vec3.fromValues(10, 1, 10));
+  var beachObject = new RenderObject(
+    mat4.create(),
+    vec4.fromValues(1, 1, 0, 1),
+    defaultProgram,
+    beachMesh.vertexBuffer,
+    beachMesh.indexBuffer.numItems);
 
-	// Create water
-	waterMesh = new OBJ.Mesh(beachStr);
-	OBJ.initMeshBuffers(gl, waterMesh);
+  mat4.translate(beachObject.transform, beachObject.transform, vec3.fromValues(25, -3, -30));
+  mat4.scale(beachObject.transform, beachObject.transform, vec3.fromValues(10, 1, 10));
+  beachObject.indexBuffer = beachMesh.indexBuffer;
 
-	waterModelMatrix = mat4.create();
-	mat4.translate(waterModelMatrix, waterModelMatrix, vec3.fromValues(200, -3.1, -50));
-	mat4.scale(waterModelMatrix, waterModelMatrix, vec3.fromValues(100, 1, 20));
+  objects.push(beachObject);
 
+
+  // Create water
+  var waterStr = document.getElementById("plane.obj").innerHTML;
+  waterMesh = new OBJ.Mesh(waterStr);
+  OBJ.initMeshBuffers(gl, waterMesh);
+
+  var waterObject = new RenderObject(
+    mat4.create(),
+    vec4.fromValues(0, 0, 1, 1),
+    defaultProgram,
+    waterMesh.vertexBuffer,
+    waterMesh.indexBuffer.numItems);
+
+  mat4.translate(waterObject.transform, waterObject.transform, vec3.fromValues(200, -3.1, -50));
+  mat4.scale(waterObject.transform, waterObject.transform, vec3.fromValues(100, 1, 20));
+  waterObject.indexBuffer = waterMesh.indexBuffer;
+
+  objects.push(waterObject);
 
   // Create cylinder
   var cylinderString = document.getElementById("cylinder.obj").innerHTML;
   cylinderMesh = new OBJ.Mesh(cylinderString);
   OBJ.initMeshBuffers(gl, cylinderMesh);
 
-  cylinderModelMatrix = mat4.create();
-  mat4.translate(cylinderModelMatrix, cylinderModelMatrix, vec3.fromValues(4, 0, 0));
+  var cylinderObject = new RenderObject(
+    mat4.create(),
+    vec4.fromValues(1, 0, 1, 1),
+    defaultProgram,
+    cylinderMesh.vertexBuffer,
+    cylinderMesh.indexBuffer.numItems);
 
-  // Setup projectionMatrix (perspective)
+  mat4.translate(cylinderObject.transform, cylinderObject.transform, vec3.fromValues(4, 0, 0));
+  cylinderObject.indexBuffer = cylinderMesh.indexBuffer;
 
+  objects.push(cylinderObject);
+
+
+  // Setup projectionMatrix (perspective)*/
   var fovy = Math.PI * 0.25; // 90 degrees
   var aspectRatio = canvas.width / canvas.height;
   var nearClippingPlane = 0.5;
@@ -81,6 +104,19 @@ window.onload = function init() {
   render();
 };
 
+var RenderObject = function(transform, color, shader, buffer, bufferLength) {
+  this.transform = transform;
+  this.color = color;
+  this.shader = shader;
+  this.buffer = buffer;
+  this.bufferLength = bufferLength;
+  this.indexBuffer;
+
+  this.rotationY = 0.01;
+  this.rotationX = 0.01;
+  this.rotationZ = 0.01;
+}
+
 function render() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -91,72 +127,25 @@ function render() {
   modelMatrixLoc = gl.getUniformLocation(defaultProgram, "modelMatrix");
   colorLoc = gl.getUniformLocation(defaultProgram, "objectColor");
 
-  // Set attribute
-  var vPosition = gl.getAttribLocation(defaultProgram, "vPosition");
-  gl.bindBuffer(gl.ARRAY_BUFFER, cubeMesh.vertexBuffer);
-  gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vPosition);
 
-  // Set uniforms
-  gl.uniformMatrix4fv(projectionMatrixLoc, false, projectionMatrix);
-  gl.uniformMatrix4fv(viewMatrixLoc, false, viewMatrix);
-  gl.uniformMatrix4fv(modelMatrixLoc, false, cubeModelMatrix);
-  gl.uniform4fv(colorLoc, vec4.fromValues(0, 1, 0, 1));
+  objects.forEach(function(object) {
 
-  // Draw
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeMesh.indexBuffer);
-  gl.drawElements(gl.TRIANGLES, cubeMesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+    // Set attribute
+    var vPosition = gl.getAttribLocation(object.shader, "vPosition");
+    gl.bindBuffer(gl.ARRAY_BUFFER, object.buffer);
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
 
-  // Set attribute
-  var vPosition2 = gl.getAttribLocation(defaultProgram, "vPosition");
-  gl.bindBuffer(gl.ARRAY_BUFFER, cylinderMesh.vertexBuffer);
-  gl.vertexAttribPointer(vPosition2, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vPosition2);
+    // Set uniforms
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, projectionMatrix);
+    gl.uniformMatrix4fv(viewMatrixLoc, false, viewMatrix);
+    gl.uniformMatrix4fv(modelMatrixLoc, false, object.transform);
+    gl.uniform4fv(colorLoc, object.color);
 
-  // Set uniforms
-  gl.uniformMatrix4fv(projectionMatrixLoc, false, projectionMatrix);
-  gl.uniformMatrix4fv(viewMatrixLoc, false, viewMatrix);
-  gl.uniformMatrix4fv(modelMatrixLoc, false, cylinderModelMatrix);
-  gl.uniform4fv(colorLoc, vec4.fromValues(1, 0, 0, 1));
-
-  // Draw
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cylinderMesh.indexBuffer);
-  gl.drawElements(gl.TRIANGLES, cylinderMesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
-
-
-  // Set attribute
-  var vPosition3 = gl.getAttribLocation(defaultProgram, "vPosition");
-  gl.bindBuffer(gl.ARRAY_BUFFER, beachMesh.vertexBuffer);
-  gl.vertexAttribPointer(vPosition3, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vPosition3);
-
-  // Set uniforms
-  gl.uniformMatrix4fv(projectionMatrixLoc, false, projectionMatrix);
-  gl.uniformMatrix4fv(viewMatrixLoc, false, viewMatrix);
-  gl.uniformMatrix4fv(modelMatrixLoc, false, beachModelMatrix);
-  gl.uniform4fv(colorLoc, vec4.fromValues(1, 1, 0, 1));
-
-  // Draw
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, beachMesh.indexBuffer);
-  gl.drawElements(gl.TRIANGLES, beachMesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
-
-
-	// Set attribute
-  var vPosition3 = gl.getAttribLocation(defaultProgram, "vPosition");
-  gl.bindBuffer(gl.ARRAY_BUFFER, waterMesh.vertexBuffer);
-  gl.vertexAttribPointer(vPosition3, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vPosition3);
-
-  // Set uniforms
-  gl.uniformMatrix4fv(projectionMatrixLoc, false, projectionMatrix);
-  gl.uniformMatrix4fv(viewMatrixLoc, false, viewMatrix);
-  gl.uniformMatrix4fv(modelMatrixLoc, false, waterModelMatrix);
-  gl.uniform4fv(colorLoc, vec4.fromValues(0, 0, 1, 1));
-
-  // Draw
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, waterMesh.indexBuffer);
-  gl.drawElements(gl.TRIANGLES, waterMesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
-
+    // Draw
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, object.indexBuffer);
+    gl.drawElements(gl.TRIANGLES, object.bufferLength, gl.UNSIGNED_SHORT, 0);
+  });
 
   requestAnimFrame(render);
 }
